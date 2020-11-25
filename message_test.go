542f101f8e6136290945b163cb60b5f4f12313a2
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2020-11-25 17:52:20 +0300

    message test codes add

---
+++ message_test.go
@@ -0,0 +1,29 @@
+package goch_test
+
+import (
+	"github.com/ribice/goch"
+	"reflect"
+	"testing"
+
+
+)
+
+func TestEncodeMSG(t *testing.T) {
+	m := &goch.Message{Time: 123, Seq: 1, Text: "Hello World", FromUID: "ABC", FromName: "User1"}
+	bts, err := m.Encode()
+	if err != nil {
+		t.Errorf("did not expect error but received: %v", err)
+	}
+	msg, err := goch.DecodeMsg(bts)
+	if err != nil {
+		t.Errorf("did not expect error but received: %v", err)
+	}
+	if !reflect.DeepEqual(m, msg) {
+		t.Errorf("expected msg %v but got %v", m, msg)
+	}
+
+	_, err = goch.DecodeMsg([]byte("msg"))
+	if err == nil {
+		t.Error("expected error but received nil")
+	}
+}
